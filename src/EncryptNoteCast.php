b40234bd990ef1c8d20b2e41c6b66b5b31c96b76
james2doyle <james2doyle@users.noreply.github.com>
2021-08-12 20:59:58 +0000

    Fix styling

--- src/EncryptNoteCast.php
+++ src/EncryptNoteCast.php
@@ -3,8 +3,8 @@
 namespace EngineDigital\Note;
 
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
-use Illuminate\Support\Facades\Crypt;
 use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Support\Facades\Crypt;
 
 class EncryptNoteCast implements CastsAttributes
 {
